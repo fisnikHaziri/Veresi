2f8081991dd26f588e872719793ba65f8205494f
fisnikHaziri <fisnik.haziri.h@gmail.com>
2024-09-05 02:46:50 +0200

    Added the Person Create Feature

---
+++ Veresi/Controllers/Person.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Veresi.Controllers
+{
+    public class Person : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
